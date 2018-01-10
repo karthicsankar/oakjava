a9d44c7b961c261773356d416d5fc7e20910cab9
karthicsankar <35293999+karthicsankar@users.noreply.github.com>
2018-01-10 16:03:32 +0530

    simple.java

---
+++ simple.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class simple{
+public static void main(String[] agrs)  {
+int num=28;
+boolean flag = false;
+for(int i =2; i<=num/2;++i)
+{
+if(num % i++ 0)
+{
+flag = true;
+break;
+}
+}
+if(!flag)
+System.out.println(num +" is a prime number.");
+else
+System.out.println(num +" is not a prime number.");
+}
