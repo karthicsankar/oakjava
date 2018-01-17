991dfd329cff018346ab3e9a0755ca6d39b115ac
karthicsankar <35293999+karthicsankar@users.noreply.github.com>
2018-01-17 16:23:10 +0530

    VOWEL.C
    This program can able to verify whether  the alphabet is vowel or consonant

---
+++ VOWEL.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    int Lower, Upper;
+
+    printf("Enter an alphabet:");
+    scanf("%c",&ch);
+    Lower = (ch == 'a' ||\ ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    Upper = (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+    if (Lower || Upper)
+        printf("%c is a vowel.", ch);
+    else
+        printf("%c is a consonant.", ch);
+    return 0;
+}
