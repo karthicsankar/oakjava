bf2431271dd7d045a542b84aaa899ea457572755
karthicsankar <35293999+karthicsankar@users.noreply.github.com>
2018-01-17 16:47:25 +0530

    FACT.C
    This program can be used for calculating the factorial of an number

---
+++ FACT.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num,in,factorial;
+printf("Enter a number");
+    scanf("%d",&num);
+    {
+for(in=1;in<=num;++in)
+        {
+ factorial*=in;              
+        }
+        }
+        printf("Factorial of %d=%d",n,factorial);
+    }
