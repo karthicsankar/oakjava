92441ff82da16f26fa0f4fcd1e5f29734dbfe337
karthicsankar <35293999+karthicsankar@users.noreply.github.com>
2018-02-07 16:42:38 +0530

    count_variable.c

---
+++ count_variable.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int in,cn=0;
+	char k[100];
+	for(in=0;in<=50;in++)
+	{
+		scanf("%c",&k[in]);
+	}
+	for(in=0;in<=k[in];in++)
+	{
+		if((k[in]>='a'&&k[in]<='z')||(k[in]>='A'&&k[in]<='Z'))
+		{
+		c++;
+	}}
+	printf("%d",c);
+return 0;
+}
