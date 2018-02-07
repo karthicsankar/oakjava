ff8444fb52b984783601bc08ed7c480a46218eb2
karthicsankar <35293999+karthicsankar@users.noreply.github.com>
2018-02-07 16:13:38 +0530

    medsort.c

---
+++ medsort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+                                                       
+  int main( )                                                     
+   {                                                           
+       int i,j,n;                                              
+       float med,a[N],t;                                   
+       printf("Enter the number of items\n");                  
+       scanf("%d", &n);                                       
+       printf("Input %d values \n",n);                         
+       for (i = 1; i <= n ; i++)                               
+           scanf("%f", &a[i]);                           
+for (i = 1 ; i <= n-1 ; i++)                            
+       {     
+for (j = 1 ; j <= n-i ; j++)                         
+          {                                                    
+              if (a[j] <= a[j+1])                              
+              {                 t = a[j];                                      
+                a[j] = a[j+1];                                 
+                a[j+1] = t;                                    
+              }                                                
+              elsecontinue ;                                     
+          }                                                    
+       } 
+if ( n % 2 == 0)                                        
+          med = (a[n/2] + a[n/2+1])/2.0 ;                   
+       else                                                    
+          med = a[n/2 + 1];                     
+for (i = 1 ; i <= n ; i++)                              
+           printf("%f  ", a[i]);                               
+       printf("\n\nMedian is %f\n", med);  
+       return 0;
+                                                               
+} 
